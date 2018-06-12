1a7cb6245e67587da971163d4a951c161e7062c8
Anthony Price <sevprice@gmail.com>
2018-06-11 21:21:02 -0400

    Updated to 6.0.3

--- src/app/pages/about/aboutRoutingModule.ts
+++ src/app/pages/about/aboutRoutingModule.ts
@@ -2,7 +2,6 @@
 import { NgModule } from '@angular/core';
 import { Routes,
          RouterModule } from '@angular/router';
-/*          import {AuthorizationGuard as AuthGuard } from '../Services/authorization.guard';, canActivate: [AuthGuard]  */
          import { AboutComponent } from './about.component';
 const routes: Routes = [
   { path: '',
@@ -16,7 +15,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
-  // providers: [AuthGuard]
 })
 
 export class AboutRoutingModule {}
